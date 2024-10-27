0e6986a66526c8972557bf3c7e75105e988ebd77
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-10-27 13:50:02 +0500

    make repl output parser output

--- repl/repl.go
+++ repl/repl.go
@@ -6,10 +6,21 @@ import (
 	"io"
 
 	"github.com/ahmadjavaidwork/coffee-int/lexer"
-	"github.com/ahmadjavaidwork/coffee-int/token"
+	"github.com/ahmadjavaidwork/coffee-int/parser"
 )
 
 const PROMPT = ">> "
+const COFFE_CUP = `
+      )  (
+     (   ) )
+      ) ( (
+ mrf_______)_
+ .-'---------|  
+( C|/\/\/\/\/|
+ '-./\/\/\/\/|
+   '_________'
+    '-------'
+`
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -19,11 +30,27 @@ func Start(in io.Reader, out io.Writer) {
 		if !scanned {
 			return
 		}
-		line := scanner.Text()
 
+		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	io.WriteString(out, COFFE_CUP)
+	io.WriteString(out, "Woops! We ran into an error here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
